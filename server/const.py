ba7a3318a39ff58d9cf4fee6341ad1c2d292a8b0
Apellun <111857797+Apellun@users.noreply.github.com>
2024-02-13 20:09:14 +0300

    paths

--- server/const.py
+++ server/const.py
@@ -1,1 +1,6 @@
-DATABASE_PATH = "server/database.db"
+import sys
+from pathlib import Path
+
+BASE_DIR_STR = Path(__file__).resolve().parent
+# BASE_DIR_STR = Path(sys.executable)
+DATABASE_PATH = BASE_DIR_STR / "database.db"
