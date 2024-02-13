1b1be290c3d3c1629cb276a0b1611ec3e7d9c8a8
apellun <mcinnie@gmail.com>
2024-02-13 23:18:14 +0300

    tests_fix

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -7,20 +7,20 @@ class TestServer:
             mock_cursor = mock_connect.return_value.__enter__.return_value.cursor
             mock_execute = mock_cursor.return_value.execute
 
-            response = client.post('/post', json={"datetime": "20240101 12:00:00", "content": "Test Content", "push_num": 1})
+            response = client.post('/post', json={"created_datetime": "20240101 12:00:00", "content": "Test Content", "push_num": 1})
             
             assert mock_execute.called
             assert response.status_code == 200
             
     def test_incorrect_post_request(self, client):
-        response = client.post('/post', json={"datetime": "20240101 12:00:00", "content": "Test Content", "push_num": "1"})
+        response = client.post('/post', json={"created_datetime": "20240101 12:00:00", "content": "Test Content", "push_num": "1"})
         assert response.status_code == 400
 
     def test_get_request_response(self, client):
         with patch('server.views.get_view.sqlite3') as mock_sqlite:
             mock_connection = mock_sqlite.connect.return_value.__enter__.return_value
             mock_cursor = mock_connection.cursor.return_value
-            mock_cursor.fetchall.return_value = [("2024-01-01 12:00:00", "Test Content", 1)]
+            mock_cursor.fetchall.return_value = [{"created_datetime": "2024-01-01 12:00:00", "content": "Test Content", "push_num": 1}]
 
             response = client.get('/get')
             
